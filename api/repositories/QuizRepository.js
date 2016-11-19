75b98d730f44a060fe40880c032b08e70080d9b8
Arne Van Raepenbusch <arvraepe@gmail.com>
2016-11-19 19:15:15 +0100

    Added display mode for viewing the scores in a certain way

--- api/repositories/QuizRepository.js
+++ api/repositories/QuizRepository.js
@@ -15,6 +15,8 @@ const scoresForTeam = (team) => Lenses.create(['scores', team]);
 const teams = () => Lenses.create(['teams']);
 const rounds = () => Lenses.create(['rounds']);
 const scores = () => Lenses.create(['scores']);
+const display = () => Lenses.create(['display']);
+const displayMode = () => Lenses.create(['display', 'mode']);
 
 // Getters
 const getAllTeams = () => teams().view();
@@ -22,11 +24,14 @@ const getAllRounds = () => rounds().view();
 const getAllScores = () => scores().view();
 const getScoresForTeam = (team) => scoresForTeam(team).view();
 const getScoreForTeamForRound = (team, round) => scoresForTeamForRound(team, round).view();
+const getDisplay = () => display().view();
+const getDisplayMode = () => displayMode().view();
 
 // Setters
 const setTeams = (t) => teams().set(t);
 const setRounds = (r) => rounds().set(r);
 const setScoreForTeamForRound = (team, round, value) => scoresForTeamForRound(team, round).set(value);
+const setDisplayMode = (mode) => displayMode().set(mode);
 
 // Creators
 const createTeam = (team) => {
@@ -61,8 +66,8 @@ const save = () => fs.writeFileSync(dir + '/api/data/data.json', JSON.stringify(
 
 // Exports
 module.exports = {
-    getAllTeams, getAllRounds, getAllScores, getScoresForTeam, getScoreForTeamForRound,
-    setScoreForTeamForRound,
+    getAllTeams, getAllRounds, getAllScores, getScoresForTeam, getScoreForTeamForRound, getDisplay, getDisplayMode,
+    setScoreForTeamForRound, setDisplayMode,
     save,
     createTeam, createRound,
     deleteTeam, deleteRound
