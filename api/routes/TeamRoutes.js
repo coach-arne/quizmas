8349a5a4bd9f4d96f2e485aabc289302b0e1ec39
Arne Van Raepenbusch <arne.vanraepenbusch@jstack.eu>
2018-12-22 11:49:11 +0100

    Updates for 2018 with team names and initial save

--- api/routes/TeamRoutes.js
+++ api/routes/TeamRoutes.js
@@ -2,6 +2,11 @@
  * Created by Arne on 19-11-2016.
  */
 
+const Multer = require('multer');
+const Upload = Multer({ dest: 'uploads/' });
+
+const CSV = require("csvtojson");
+
 const QuizRepository = require('repositories/QuizRepository');
 const QuizService = require('services/QuizService');
 
@@ -12,4 +17,21 @@ module.exports = (App) => {
     App.delete('/api/team/:team', (req, res) => res.status(200).send(QuizService.deleteTeam(req.params.team)));
     App.get('/api/team/:team/score/list', (req, res) => res.status(200).send(QuizRepository.getScoresForTeam(req.params.team)));
     App.post('/api/team/:team/tiebreaker', (req, res)=> res.status(200).send(QuizRepository.setTiebreaker(req.params.team, req.body.value)));
+
+    App.post('/api/teams/import', Upload.single('csv'), async (req, res) => {
+
+        try {
+            const teams = await CSV({delimiter: ';'}).fromFile(req.file.path);
+
+            teams
+                .filter(({Nr}) => !Number.isNaN(parseInt(Nr)))
+                .forEach(({Nr, Teamnaam}) => {
+                    QuizService.createTeam(Teamnaam, Nr);
+                });
+
+            res.status(200).send({message: "import done"});
+        } catch(error) {
+            res.status(500).send({message: "Something wrong, see logs"});
+        }
+    });
 };
